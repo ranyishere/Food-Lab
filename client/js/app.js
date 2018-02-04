3c413a73e0a311aac66e8e2c5921018f2baace60
ghalander <hector.med27@gmail.com>
2018-02-04 11:26:08 -0800

    Created the form but still need to connect to database. Setup django FoodLab App

--- client/js/app.js
+++ client/js/app.js
@@ -5,12 +5,16 @@ angular.module('foodlab', [
 	'ngRoute',
 	'ngAnimate',
 	'foodlab.controllers',
+	'foodlab.directives',
 	'foodlab.memoryServices',
+	'foodlab'
 ])
 	.config(['$routeProvider', function($routeProvider)  {
-		$routeProvider.when('/restaurants', {templateUrl: 'partials/restaurant-list.html', controller: 'RestaurantListCtrl'});
-		$routeProvider.when('/restaurants/:restaurantId', {templateUrl: 'partials/restaurant-detail.html', controller: 'RestaurantDetailCtrl'});
-		$routeProvider.when('/login',{templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
-		$routeProvider.when('/map', {templateUrl: 'partials/map.html', controller: 'MapCtrl'});
-		$routeProvider.otherwise({redirectTo: '/restaurants'});
+		$routeProvider
+			.when('/restaurants', {templateUrl: 'partials/restaurant-list.html', controller: 'RestaurantListCtrl'})
+			.when('/restaurants/:restaurantId', {templateUrl: 'partials/restaurant-detail.html', controller: 'RestaurantDetailCtrl'})
+			.when('/login',{templateUrl: 'partials/login.html', controller: 'LoginCtrl'})
+			.when('/map', {templateUrl: 'partials/map.html', controller: 'MapCtrl'})
+			.when('/cms', {templateUrl:'partials/restaurant-cms.html', controller:'ContentManagerCtrl'})
+			.otherwise({redirectTo: '/restaurants'});
 	}]);
--- client/js/directives.js
+++ client/js/directives.js
@@ -51,17 +51,65 @@ angular.module('angularProject.directives', ['http-auth-interceptor'])
 		                "username": scope.username,
 		                "password": scope.password,
 		            };
-					
+
 					//$http.post(constants.serverAddress + "api-token-auth", user_data)
 		            $http.post("http://localhost:8001/api-token-auth/", user_data)
 		                .success(function(response) {
 		                	$cookieStore.put('djangotoken', response.token);
 		                    $http.defaults.headers.common['Authorization'] = 'Token ' + response.token;
 		                    authService.loginConfirmed();
-		            }); 
+		            });
 
  				});
 
  			}
  		}
  	});
+	// TODO: (Hector) adding controller for cms list
+	//This directive is to DYNAMICALLY update the food listing
+angular.module('foodlab.directives', [])
+	.directive('foodDirective',  function($compile) {
+			return {
+					template:
+					'<hr/>'+
+					'<div class="form-row">'+
+					'<div class="form-group col-md-6">'+
+					'<label>Food Name</label>' +
+					'<input class="form-control" type="text" ng-model="food.name" placeholder="Food Name"/>'+
+					'</div>'+
+					'<div class="form-group col-md-6">'+
+					'<label>Food Price</label>'+
+					'<input class="form-control" type="text" ng-model="food.price" placeholder="Food Price"/>'+
+					'</div>'+
+					'</div>'+
+					'<div class="form-row">'+
+					'<div class="form-group col-md-12">'+
+					'<label>Food Description</label>'+
+					'<input class="form-control" type="text" ng-model="food.description" placeholder="Food Description"/>'+
+					'</div></div>',
+					link:  function($scope, element, attr) {
+						$scope.addFoodList = function() {
+							$scope.foodNumberListing++;
+      				var appendList = angular.element(
+								'<hr/>'+
+								'<div class="form-row">'+
+								'<div class="form-group col-md-6">'+
+								'<label>Food Name '+($scope.foodNumberListing+1)+':</label>' +
+								'<input class="form-control" type="text" ng-model="food.name'+$scope.foodNumberListing+'" placeholder="Food Name '+($scope.foodNumberListing+1)+'"/>'+
+								'</div>'+
+								'<div class="form-group col-md-6">'+
+								'<label>Food Price '+($scope.foodNumberListing+1)+'</label>'+
+								'<input class="form-control" type="text" ng-model="food.price'+$scope.foodNumberListing+'" placeholder="Food Price '+($scope.foodNumberListing+1)+'"/>'+
+								'</div>'+
+								'</div>'+
+								'<div class="form-row">'+
+								'<div class="form-group col-md-12">'+
+								'<label>Food Description '+($scope.foodNumberListing+1)+'</label>'+
+								'<input class="form-control" type="text" ng-model="food.description'+$scope.foodNumberListing+'" placeholder="Food Description '+($scope.foodNumberListing+1)+'"/>'+
+								'</div></div>'
+							);
+              element.append(appendList);
+						}
+         }
+			 };
+	});
